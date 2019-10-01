0b6c58909dccba95fad895031ebecb8efa4feaec
Jansher Marri <janshermarri@yahoo.com>
2019-10-01 19:17:29 +0500

    Tested permissions, made changes to GradeViewSet to implement CRUD.

--- academics/views.py
+++ academics/views.py
@@ -1,14 +1,22 @@
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from academics import models, serializers
-from common import permissions
+from academics import models, serializers, permissions
+from common import permissions as common_permissions
+from common.permissions import IsAdmin, IsTeacher
 
 
 class SubjectViewSet(ModelViewSet):
     queryset = models.Subject.objects.filter(is_active=True)
     serializer_class = serializers.SubjectSerializer
-    permission_classes = (permissions.IsAdmin,)
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsAdmin | IsTeacher]
+        else:
+            permission_classes = [IsAdmin]
+        return [permission_class() for permission_class in permission_classes]
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
--- structure/views.py
+++ structure/views.py
@@ -1,11 +1,20 @@
 from rest_framework.mixins import ListModelMixin
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+
 
 from common.permissions import IsAdmin
 from structure import models, serializers
 
 
-class GradeViewSet(ListModelMixin, GenericViewSet):
-    queryset = models.Grade.objects.all().prefetch_related('sections')
+class GradeViewSet(ModelViewSet):
+    queryset = models.Grade.objects.filter(is_active=True).prefetch_related('sections')
     permission_classes = (IsAdmin,)
     serializer_class = serializers.GradeSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
